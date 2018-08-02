3872ee7bd4b2076f95aef31243d5b593adf9d125
Maria Szczepańska <szczepanskamaria93@gmail.com>
2018-08-02 19:59:53 +0200

    Add Bird and BirdPhoto models.

--- bird_app/models.py
+++ bird_app/models.py
@@ -1,3 +1,41 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Bird(models.Model):
+    DISTRIBUTION_CHOICES = (
+        ('north', 'north'),
+        ('south', 'south'),
+        ('west', 'west'),
+        ('east', 'east'),
+        ('center', 'center')
+    )
+    HABITAT_CHOICES = (
+        ('mountains', 'mountains'),
+        ('open plains', 'open plains'),
+        ('forests', 'forests'),
+        ('rivers and lakes', 'rivers and lakes'),
+        ('the seasides', 'the seasides'),
+        ('countries and hamlets', 'countries and hamlets'),
+        ('urban', 'urban'),
+        ('parks and gardens', 'parks and gardens')
+    )
+
+    latin_name = models.CharField(max_length=settings.MAX_CHAR_LENGTH)
+    eng_name = models.CharField(max_length=settings.MAX_CHAR_LENGTH)
+    family = models.CharField(max_length=settings.MAX_CHAR_LENGTH)
+    order = models.CharField(max_length=settings.MAX_CHAR_LENGTH)
+    distribution = models.CharField(
+        max_length=6,
+        choices=DISTRIBUTION_CHOICES
+    )
+    habitat = models.CharField(
+        max_length=settings.MAX_CHAR_LENGTH,
+        choices=HABITAT_CHOICES
+    )
+
+
+# TODO: add related_name 'photos' to foreign key
+class BirdPhoto(models.Model):
+    bird = models.ForeignKey('Bird', on_delete=models.CASCADE)
+    image_url = models.URLField(max_length=settings.MAX_CHAR_LENGTH)
--- bird_project/settings.py
+++ bird_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -82,6 +81,7 @@ DATABASES = {
     }
 }
 
+MAX_CHAR_LENGTH = 255
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
